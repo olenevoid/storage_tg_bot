4c2858024b3fb218bca971057d352aa3618c0648
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-07-25 19:22:05 +0500

    перенес файл с бд в папку джанго

--- bot_django_app/bot_db.py
+++ bot_django_app/bot_db.py
@@ -1,5 +1,5 @@
 # сюда функции для работы с бд джанго
-from bot.models import Client
+from bot_django_app.models import Client
 
 def get_user(id_user):
     return Client.objects.get(telegram_id=id_user)
