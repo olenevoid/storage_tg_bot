4c8757114fef09a85d80b605a82c7eb0c90eabb7
olenevoid <arnafas@gmail.com>
2025-07-27 18:10:41 +0500

    Добавил валидаторы

---
+++ ptb/validators.py
@@ -0,0 +1,12 @@
+
+
+def name_is_valid(full_name: str) -> bool:
+    return len(full_name) > 2
+
+
+def phone_is_valid(phone: str) -> bool:
+    return len(phone) > 2
+
+
+def email_is_valid(email: str) -> bool:
+    return len(email) > 2
