7327ae4e6bd772439921c8e0b5ff95f3b0baf64d
olenevoid <arnafas@gmail.com>
2025-07-28 17:25:17 +0500

    Добавил валидатор для срока аренды

--- ptb/validators.py
+++ ptb/validators.py
@@ -1,3 +1,4 @@
+from ptb.settings import MAX_RENT_PERIOD
 
 
 def name_is_valid(full_name: str) -> bool:
@@ -10,3 +11,7 @@ def phone_is_valid(phone: str) -> bool:
 
 def email_is_valid(email: str) -> bool:
     return len(email) > 2
+
+
+def period_is_valid(period: str) -> bool:
+    return period.isnumeric() and int(period) <= MAX_RENT_PERIOD
